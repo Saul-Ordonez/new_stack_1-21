3eec7462ff7077625f544923dc3756c0858d0adf
Student <example@example.com>
2020-01-21 16:35:07 -0800

    fix new question route bug

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,55 +1,56 @@
 class QuestionsController < ApplicationController
-    before_action :authorize, only: [:secret]
+  before_action :authorize, only: [:secret]
   def index
-     @questions = Question.all
-     render :index
-   end
+    @questions = Question.all
+    render :index
+  end
 
-    def new
-      @user = User.find(params[:user_id])
-      @question = @user.questions.new
+  def new
+    @question = Question.new
+    render :new
+  end
+
+  def create
+    # @user = User.find(params[:user_id])
+    @question = Question.new(question_params)
+    # @question = @user.questions.new
+    if @question.save
+      redirect_to user_path(@user)
+    else
       render :new
     end
-    def create
-        @user = User.find(params[:user_id])
-        @question = @user.questions.new(question_params)
-        if @question.save
-          redirect_to user_path(@user)
-        else
-          render :new
-        end
-      end
-
-      def show
-        @user = User.find(params[:user_id])
-        @question = Question.find(params[:id])
-        render :show
-      end
-
-      def edit
-        @user = User.find(params[:user_id])
-        @question = Question.find(params[:id])
-        render :edit
-      end
-
-      def update
-        @question = Question.find(params[:id])
-        if @question.update(question_params)
-          redirect_to user_path(@question.user)
-        else
-          render :edit
-        end
-      end
-
-      def destroy
-        @question = Question.find(params[:id])
-        @question.destroy
-        redirect_to user_path(@question.user)
-      end
-
-      private
-        def question_params
-          params.require(:question).permit(:content, :user_id)
-        end
-
   end
+
+  def show
+    @user = User.find(params[:user_id])
+    @question = Question.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find(params[:user_id])
+    @question = Question.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to user_path(@question.user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to user_path(@question.user)
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:content, :user_id)
+  end
+
+end
